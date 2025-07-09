31f65c9de56370af63b7de2fe34cf029cdc2ba00
Bikrant Thakur <57788465+Bikrant226@users.noreply.github.com>
2025-07-09 21:24:53 +0530

    SW-few-more-questions

---
+++ Sliding Window/first_even_number.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<list>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int arr[]={3,2,4,-7,5,7,8};
+    list<int> l;
+    vector<int> vec;
+    int k=3,i=0,j=0,num=0;
+
+    while(j<sizeof(arr)/sizeof(arr[0])){
+        if(arr[j]%2==0){
+            l.push_back(arr[j]);
+        }
+        if(j-i+1<k)
+            j++;
+        else if(j-i+1==k){
+            if(l.size()==0){
+                vec.push_back(0);
+            }
+            else{
+                vec.push_back(l.front());
+                if(arr[i]==l.front())
+                   { l.pop_front();}
+            }
+            i++;
+            j++;
+        }    
+    }
+    for(auto a: vec){
+        cout<<a<<"\n";
+    }
+
+    return 0;
+}
